12f2b6b50737a0af045fdf9f74e2ab1e0d23b1a4
MauriTiitinen <mauri.a.tiitinen@gmail.com>
2025-02-16 15:55:32 +0200

    UI

--- Projekti/Projekti/MainWindow.xaml.cs
+++ Projekti/Projekti/MainWindow.xaml.cs
@@ -20,5 +20,20 @@ namespace Projekti
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
